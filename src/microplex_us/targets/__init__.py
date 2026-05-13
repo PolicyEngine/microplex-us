6d53b24a6772caf5c93710f828f56ca5ce5d870f
Max Ghenis <mghenis@gmail.com>
2026-05-12 21:14:44 -0400

    Add ACA PTC multiplier source builder

--- src/microplex_us/targets/__init__.py
+++ src/microplex_us/targets/__init__.py
@@ -1,5 +1,16 @@
 """US-specific target mappings."""
 
+from microplex_us.targets.aca_ptc import (
+    ACA_AVERAGE_MONTHLY_APTC_CONCEPT,
+    ACA_MARKETPLACE_EFFECTUATED_ENROLLMENT_CONCEPT,
+    ACAPTCBaseAPTCPolicy,
+    ACAPTCMultiplierInput,
+    ACAPTCMultiplierRow,
+    aca_ptc_multiplier_inputs_from_arch_consumer_facts,
+    build_aca_ptc_multiplier_rows,
+    load_arch_consumer_fact_jsonl_rows,
+    write_policyengine_aca_ptc_multiplier_csv,
+)
 from microplex_us.targets.adapters import (
     POLICYENGINE_US_COUNT_ENTITIES,
     policyengine_db_target_to_canonical_spec,
@@ -19,6 +30,15 @@ __all__ = [
     "POLICYENGINE_US_COUNT_ENTITIES",
     "policyengine_db_target_to_canonical_spec",
     "policyengine_db_targets_to_canonical_set",
+    "ACA_AVERAGE_MONTHLY_APTC_CONCEPT",
+    "ACA_MARKETPLACE_EFFECTUATED_ENROLLMENT_CONCEPT",
+    "ACAPTCBaseAPTCPolicy",
+    "ACAPTCMultiplierInput",
+    "ACAPTCMultiplierRow",
+    "aca_ptc_multiplier_inputs_from_arch_consumer_facts",
+    "build_aca_ptc_multiplier_rows",
+    "load_arch_consumer_fact_jsonl_rows",
+    "write_policyengine_aca_ptc_multiplier_csv",
     "RACVariable",
     "RAC_VARIABLE_MAP",
     "POLICYENGINE_TO_RAC",
